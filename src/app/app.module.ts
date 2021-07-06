cd6d72bcca5484d159619747bf903f955c79183b
Yeferson Castiblanco <yeferson@freelanders.co>
2021-07-05 21:59:41 -0500

    se agrego las credenciales de firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,6 +6,11 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/componets/header/header.component';
 import { HeaderModule } from './shared/componets/header/header.module';
 import { EmployeeFormModule } from './shared/componets/employee-form/employee-form.module';
+
+// import de firebase
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule} from '@angular/fire/firestore'
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -17,9 +21,11 @@ import { EmployeeFormModule } from './shared/componets/employee-form/employee-fo
     BrowserModule,
     AppRoutingModule,
     HeaderModule,
-    EmployeeFormModule
+    EmployeeFormModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    
   ],
-  providers: [],
+  providers: [AngularFirestoreModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyB612lVDyyiYokTAdQ5sw0SNw2kTtbV1AA",
+    authDomain: "crudemployee-4c77f.firebaseapp.com",
+    projectId: "crudemployee-4c77f",
+    storageBucket: "crudemployee-4c77f.appspot.com",
+    messagingSenderId: "1021571030469",
+    appId: "1:1021571030469:web:4a64dd7cbd513dad523fac"
+  }
 };
